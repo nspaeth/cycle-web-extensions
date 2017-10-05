1ea42067f8d45948689f88cab4be1b150c95912e
Nathan Spaeth <nlspaeth@uwm.edu>
2017-10-05 15:39:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# `cycle-web-extensions` #
+
+Cycle.js drivers to use in Chrome extensions and WebExtensions.
+
+## Drivers ##
+
+- [`MessagesDriver`](#MessagesDriver)
+- [`WindowsDriver`](#WindowsDriver)
+- [`TabsDriver`](#TabsDriver)
+
+
+### `MessagesDriver` ###
+
+In the Web Extension architecture, logic and state both live in a long-running background page.  The user interface lives in a separate page called a popup.  The two pages communicate by passing messages between each other.  The background page awaits connections; the popup initiates them.
+
+`messagesDriver` accepts a stream of messages to send to the other page and returns a stream of messages received from the other page.
+
+`makeMessagesDriver` takes a single named argument, `shouldInitiate`.  Set it to `false` in the background page and `true` in any other pages.
+
+Here's an example of a simple counter.  Notice that when the popup opens, it sends a request for the current state to the background page.  The response it receives creates the first frame of DOM.
+
+```javascript
+import { run } from '@cycle/run'
+import xs from 'xstream'
+import sampleCombine from 'xstream/extra/sampleCombine'
+
+import {
+	makeMessagesDriver,
+	newMessage,
+	pickType,
+} from 'cycle-web-extensions'
+
+function Background({ messages: messages$ }) {
+	const initialState = { count: 0 }
+
+	const state$ = messages$.reduce(
+		(state, message) => {
+			if (message.type === 'increment') { return { ...state, count: state.count + 1 } }
+			if (message.type === 'decrement') { return { ...state, count: state.count - 1 } }
+			return state
+		},
+		initialState,
+	)
+
+	return {
+		messages: xs.merge(
+			state$.map(state => newMessage(['state_changed', state])),
+			pickType(messages$, 'current_state_requested')
+				.compose(sampleCombine(state$))
+				.map(([, state]) => newMessage(['state_changed'], state))
+			),
+	}
+}
+
+run(
+	Background,
+	{ messages: makeMessagesDriver({ shouldInitiate: false }) },
+)
+
+// and in your popup script
+import {
+	makeMessagesDriver,
+	newMessage,
+} from 'cycle-web-extensions'
+import { button, div } from 'hyperscript-helpers'
+
+function Popup({ DOM, messages: messages$ }) {
+	const count$ = pickType(messages$, 'state_changed')
+		.map(payload => payload.count) // Why no pluck in xstream?
+
+	return {
+		messages: xs.merge(
+			DOM.select('.increment').events('click').mapTo(newMessage(['increment'])),
+			DOM.select('.decrement').events('click').mapTo(newMessage(['decrement'])),
+		).startWith(newMessage('current_state_requested')),
+		DOM: count$.map(
+			count => (
+				div([
+					`The count is now ${count}.`,
+					div([button('.decrement', ['-1']), button('.increment', ['+1'])]),
+				])
+			),
+		),
+	}
+}
+
+function intent(DOM) {
+	const increment$ = DOM.select('.increment').events('click')
+		.mapTo(newMessage(['increment']))
+	const decrement$ = DOM.select('.decrement').events('click')
+		.mapTo(newMessage(['decrement']))
+
+	return {
+		messages: xs.merge(increment$, decrement$)
+			.startWith(newMessage(['current_state_requested'])),
+	}
+}
+
+run(
+	Popup,
+	{
+		DOM: makeDOMDriver('#root'),
+		messages: makeMessagesDriver({ shouldInitiate: true }),
+	},
+)
+```
+
+### `WindowsDriver` ###
+
+* TODO
+
+### `TabsDriver` ###
+
+* TODO
+
+To see a real extension written with these drivers, check out [tableut](https://github.com/nspaeth/tableut).
+
+## Installation ##
+
+```
+yarn add cycle-web-extensions
+```
+
+## License ##
+
+[Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ src/index.ts
@@ -0,0 +1,261 @@
+/**
+ *	@license Apache-2.0
+ *	Copyright 2016 - present The Midicast Authors. All Rights Reserved.
+ *	Modifications copyright (C) 2017 <Nathan L Spaeth nathan@spaeth.nl>
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *	use this file except in compliance with the License. You may obtain a copy
+ *	of the License at
+ *
+ *			http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *	WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *	License for the specific language governing permissions and limitations
+ *	under the License.
+ */
+
+import { adapt } from '@cycle/run/lib/adapt'
+import xs, { Listener, Producer, Stream, Subscription } from 'xstream'
+
+export type Window = browser.windows.Window
+type Tab = browser.tabs.Tab
+
+export type IMessagesDriver = (message$: Stream<IMessage>) => Stream<IMessage>
+export type IMessagesSource = Stream<IMessage>
+export interface IMessagesSink {
+	messages: Stream<IMessage>
+}
+
+/**
+ * Creates a Cycle.js driver to send and receive messages in a WebExtension.
+ *
+ * The WebExtension messaging API is asymmetric: the long-running background
+ * page awaits connections while ephemeral popup and content scripts initiate
+ * them.	Thus, set `shouldInitiate` to `true` unless `messagesDriver` is for a
+ * background page.
+ */
+export function makeMessagesDriver({ shouldInitiate }: { shouldInitiate: boolean }): IMessagesDriver {
+	/**
+	 * Accepts a stream of messages to send on the channel and returns a stream of
+	 * responses received on the channel.
+	 */
+	return (outgoingMessage$: Stream<IMessage>): Stream<IMessage> => {
+		let channel: browser.runtime.Port
+		let outgoingSubscription: Subscription
+		let connected = false
+		let observer: Listener<IMessage>
+
+		function forwardMessage(incomingMessage: IMessage) {
+			observer.next(incomingMessage)
+		}
+
+		function connectToChannel(newChannel: browser.runtime.Port) {
+			if (connected) { return }
+			connected = true
+			channel = newChannel
+			const outgoingMessageListener = {
+				next: (outgoingMessage: IMessage) => {
+					try {
+						channel.postMessage(outgoingMessage)
+					} catch (error) {
+						// console.warn(error)
+						// console.warn('Failed to send message', outgoingMessage)
+					}
+				},
+				error: () => { }, // TODO: What should happen here?
+				complete: () => { }, // TODO: What should happen here?
+			}
+
+			outgoingSubscription = outgoingMessage$.subscribe(outgoingMessageListener)
+			channel.onMessage.addListener(forwardMessage)
+			// console.log('Connected to: ', channel)
+			channel.onDisconnect.addListener(
+				(port: browser.runtime.Port) => {
+					// console.log('Disconnecting from : ', channel)
+					// TODO: update typing for onMessage.removeListener
+					(channel as any).onMessage.removeListener(forwardMessage)
+					outgoingMessage$.removeListener(outgoingMessageListener)
+					connected = false
+				},
+			)
+		}
+
+		const incoming$: Stream<IMessage> = xs.create({
+			start: listener => {
+				observer = listener
+				if (shouldInitiate) {
+					connectToChannel(browser.runtime.connect())
+				} else {
+					browser.runtime.onConnect.addListener(connectToChannel)
+				}
+			},
+			stop: () => {
+				outgoingSubscription.unsubscribe()
+				if (channel) { channel.disconnect() }
+			},
+		})
+
+		return adapt(incoming$ as any)
+	}
+}
+
+export interface IMessage {
+	type: string
+	payload: any
+}
+
+type IListenerGetter = () => Listener<IMessage>
+
+type IHandler = (...payload: any[]) => void
+interface IHandlers {
+	[k: string]: IHandler
+}
+type IcreateOutgoingHandlers = (getter: IListenerGetter) => IHandlers
+type IcreateIncomingHandlers = (listener: Listener<IMessage>) => void
+function createAPIDriver(createOutgingHandlers: IcreateOutgoingHandlers,
+	                        createIncomingHandlers: IcreateIncomingHandlers) {
+/**
+ * Accepts a stream of messages to send on the channel and returns a stream of
+ * responses received on the channel.
+ */
+	return (updateCommands$: Stream<IMessage>): Stream<IMessage> => {
+		let outgoingSubscription: Subscription
+		let observer: Listener<IMessage>
+
+		const outgoingHandlers = createOutgingHandlers(() => observer)
+		outgoingSubscription = updateCommands$.subscribe({
+			next: (update: IMessage) => {
+				const { type, payload } = update
+				if (outgoingHandlers[type]) { outgoingHandlers[type](...payload) }
+			},
+			error: () => { /* TODO: ?? */ },
+			complete: () => { /* TODO: ?? */ },
+		})
+
+		const incoming$: Stream<IMessage> = xs.create({
+			start: (listener: Listener<IMessage>) => { createIncomingHandlers(listener) },
+			stop: () => { outgoingSubscription.unsubscribe() },
+		})
+
+		return adapt(incoming$ as any)
+	}
+}
+
+export type IWindowDriver = (windows$: Stream<IMessage>) => Stream<IMessage>
+export type IWindowSource = Stream<IMessage>
+export type IWindowSink = Stream<Window>
+
+export function makeWindowDriver({ createListeners }: { createListeners: boolean }): IWindowDriver {
+	const getAll = (listener: Listener<IMessage>) => browser.windows.getAll(
+		{ populate: true }).then((...payload: any[]) =>
+			listener.next({
+				type: 'allWindows',
+				payload,
+			}))
+	const createOutgingHandlers = (getListener: IListenerGetter) => ({
+		getAll: (...payload: any[]) => getAll(getListener()),
+		create: browser.windows.create,
+		remove: browser.windows.remove,
+		update: browser.windows.update,
+	})
+
+	const createIncomingHandlers = (listener: Listener<IMessage>) => {
+		const handlers = [ 'onFocusChanged', 'onRemoved' ]
+			.map(event => ({ [event]: (...payload: any[]) => listener.next(newMessage(event, payload)) }))
+		const events = Object.assign({}, ...handlers)
+
+		// The on browser.windows.onCreated event does not automatically
+		// populate the 'tabs' property. Do that manually here in order
+		// to make the Window object more consistent
+		events.onCreated = (window: browser.windows.Window) =>
+			browser.tabs.query({ windowId: window.id })
+			.then(
+					(tabs: browser.tabs.Tab[]) => listener.next(newMessage('onCreated', [{ ...window, tabs }])),
+				)
+
+		Object.entries(events)
+			.map(([event, handler]) => (browser as any).windows[event].addListener(handler))
+		getAll(listener)
+	}
+	return createAPIDriver(createOutgingHandlers, createIncomingHandlers)
+}
+
+export type ITabDriver = (windows$: Stream<IMessage>) => Stream<IMessage>
+export type ITabSource = Stream<IMessage>
+export type ITabSink = Stream<IMessage>
+
+interface IattachInfo {
+	newWindowId: number
+	newPosition: number
+}
+
+interface IdetachInfo {
+	oldWindowId: number
+	oldPosition: number
+}
+const tabEvents = [
+	'onCreated',
+	'onUpdated',
+	'onMoved',
+	'onSelectionChanged',
+	'onActiveChanged',
+	'onActivated',
+	'onHighlightChanged',
+	'onHighlighted',
+	'onDetached',
+	'onAttached',
+	'onRemoved',
+	'onReplaced',
+	'onZoomChange',
+]
+export function makeTabDriver({ createListeners }: { createListeners: boolean }): ITabDriver {
+	const createOutgingHandlers = (getListener: IListenerGetter) => ({
+		connect: browser.tabs.connect,
+		sendMessage: browser.tabs.sendMessage,
+		create: browser.tabs.create,
+		duplicate: browser.tabs.duplicate,
+		update: browser.tabs.update,
+		move: browser.tabs.move,
+		reload: browser.tabs.reload,
+		remove: browser.tabs.remove,
+		executeScript: browser.tabs.executeScript,
+		insertCSS: browser.tabs.insertCSS,
+		setZoom: browser.tabs.setZoom,
+		setZoomSettings: browser.tabs.setZoomSettings,
+	})
+
+	const createIncomingHandlers = (listener: Listener<IMessage>) => {
+		const handlers = tabEvents
+			.map(event => ({
+				[event]: (...payload: any[]) => listener.next(newMessage(event, payload)),
+			}),
+		)
+		const events = Object.assign({}, ...handlers)
+
+		Object.entries(events)
+			.map(([event, handler]) => (browser as any).tabs[event].addListener(handler))
+	}
+	return createAPIDriver(createOutgingHandlers, createIncomingHandlers)
+}
+
+export type MessageType = string // 'app' | 'tabs' | 'windows'
+export function newMessage(type: MessageType | MessageType[], payload?: any): IMessage {
+	if (Array.isArray(type)) {
+		if (type.length === 0) { throw new Error('Must provide a type') }
+		const [thisType, ...types] = type
+		return {
+			type: thisType,
+			payload: types.length === 0 ? payload : newMessage(types, payload),
+		}
+	}
+
+	return { type, payload }
+}
+
+export function pickType(message$: Stream<IMessage>, type: string) {
+	return message$
+		.filter(msg => msg.type === type)
+		.map((msg: IMessage) => msg.payload)
+}
